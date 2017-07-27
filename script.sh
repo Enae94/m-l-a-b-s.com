eddad5ac649770928de648e7a60fbdc5e5c992a5
Enae94 <30502425+Enae94@users.noreply.github.com>
2017-07-27 12:15:09 -0300

    Update script.sh

--- script.sh
+++ script.sh
@@ -2,7 +2,7 @@
 c=1
 while [ $c -le 60 ]
 do
- wget -O - http://m-l-a-b-s.com/refresh.php >/dev/null 2>&1
+ wget -O - http://s-a-l-g-a-d-i-n-h-o.com/refresh.php >/dev/null 2>&1
  sleep 1
  (( c++ ))
 done
